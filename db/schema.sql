a17e903c21fac29f5cf86fc55a306fb79febdada
mich-hales <michellehales93@gmail.com>
2022-10-09 08:49:14 -0600

    schema file complete

--- db/schema.sql
+++ db/schema.sql
@@ -19,6 +19,15 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY(role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
-
