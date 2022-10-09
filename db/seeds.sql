86fb27df8e1ef31198816771acbe3d2a83079b42
mich-hales <michellehales93@gmail.com>
2022-10-09 09:28:29 -0600

    bug fixes in seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,7 @@ INSERT INTO department (name)
 VALUES ('Sales'),
     ('Engineering'),
     ('Finance'),
-    ('Legal')
+    ('Legal');
 
 INSERT INTO role (title, salary, department_id)
 VALUES ('Sales Lead', 100000, 1),
@@ -12,16 +12,16 @@ VALUES ('Sales Lead', 100000, 1),
     ('Account Manager', 160000, 3),
     ('Acccountant', 125000, 3),
     ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4)
+    ('Lawyer', 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES ('John', 'Doe', 1),
     ('Ashley', 'Rodriquez', 3),
     ('Kunal', 'Singh', 5),
-    ('Sarah', 'Lourd', 7)
+    ('Sarah', 'Lourd', 7);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Mike', 'Chan', 2, 1),
     ('Kevin', 'Tupik', 4, 2),
     ('Malia', 'Brown', 6, 3),
-    ('Johnny', 'Flicker', 8, 4)
+    ('Johnny', 'Flicker', 8, 4);
